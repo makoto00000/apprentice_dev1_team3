2c8ef9ea5373f5ed0a5a5f9dcd80069b7c65c07f
setodeve <keinick11@outlook.com>
2023-11-30 09:43:36 +0900

    Revert "Merge pull request #19 from makoto00000/revert-18-top-page"
    This reverts commit b82eefb8fe166627bce8608e36bfc3c24e433591, reversing
    changes made to 0cbdc08c8a53ba8f7d42860fb270ed7cbf6ca544.

---
+++ app/public/module/header.js
@@ -0,0 +1,76 @@
+
+const searchInput = document.getElementById('search_recipe_input')
+const placeholder = document.getElementById('place_holder')
+placeholder.addEventListener('click', function() {
+  searchInput.focus();
+})
+searchInput.addEventListener('focus', function() {
+  placeholder.style.display = 'none';
+})
+searchInput.addEventListener('blur', function() {
+  placeholder.style.display = '';
+})
+
+
+const hoverKinds = document.getElementById('kinds')
+const hoverMenuKinds = document.getElementById('kinds_hover_open')
+const hoverTastes = document.getElementById('tastes')
+const hoverMenuTastes = document.getElementById('tastes_hover_open')
+hoverMenuKinds.addEventListener ("mouseover", function() {
+  hoverKinds.style.opacity = "1"
+  hoverKinds.style.visibility = "visible"
+  hoverKinds.style.zIndex = "9999"
+  hoverTastes.style.zIndex = "1"
+  hoverTastes.style.opacity = "0"
+  hoverTastes.style.visibility = "hidden"
+
+})
+hoverKinds.addEventListener ("mouseover", function() {
+  hoverKinds.style.opacity = "1"
+  hoverKinds.style.visibility = "visible"
+})
+hoverKinds.addEventListener ("mouseout", function() {
+  hoverKinds.style.opacity = "0"
+  hoverKinds.style.visibility = "hidden"
+})
+
+hoverMenuTastes.addEventListener ("mouseover", function() {
+  hoverTastes.style.opacity = "1"
+  hoverTastes.style.visibility = "visible"
+  hoverTastes.style.zIndex = "9999"
+  hoverKinds.style.zIndex = "1"
+  hoverKinds.style.opacity = "0"
+  hoverKinds.style.visibility = "hidden"
+})
+hoverTastes.addEventListener ("mouseover", function() {
+  hoverTastes.style.opacity = "1"
+  hoverTastes.style.visibility = "visible"
+})
+hoverTastes.addEventListener ("mouseout", function() {
+  hoverTastes.style.opacity = "0"
+  hoverTastes.style.visibility = "hidden"
+})
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const isLoggedIn = await Auth.isLoggedIn();
+  writeRecipeLink = document.getElementById('write_recipe_link');
+  loginButton = document.getElementById('login_button');
+  userIcon = document.getElementById('user_icon');
+  userIconImage = document.getElementById('user_icon_image');
+
+  if (isLoggedIn) {
+    const response = await fetch('/api/get_current_user')
+    const data = await response.json()
+    user = JSON.stringify(data, null, 2)
+    userdata = JSON.parse(user)
+    writeRecipeLink.style.display = "block";
+    userIcon.style.display = "block";
+    loginButton.style.display = "none";
+    userIconImage.src = `./public/image/default_icons/${userdata.image}`;
+  } else {
+    userIcon.style.display = "none";
+    loginButton.style.display = "block";
+  }
+});
+
+// TODO: データベースに画像URLを保存して表示テスト
